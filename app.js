a7f90cfc2d93b02936120715101452fa0bb26cb9
jonathangani279 <jonathangani279@gmail.com>
2020-05-02 23:14:51 +0700

    create project

---
+++ app.js
@@ -0,0 +1,48 @@
+const db= require('./database');
+const express= require('express');
+const jwt= require('jsonwebtoken');
+const multer= require('multer');
+const path= require('path');
+const fs= require('fs');
+
+const app= express();
+const config= {
+    host: 'https://api.spoonacular.com/recipes',
+    api_key: ''
+};
+
+//MULTER
+const upload= multer({
+    storage: multer.diskStorage({
+        destination: (req, file, cb) => {
+            cb(null, './public/uploads')
+        },
+        filename: (req, file, cb) => {
+            cb(null, file.originalname.split('.')[0]+''+path.extname(file.originalname))
+        },
+    }),
+    fileFilter: (req, file, cb) => {
+        checkFileType(file, cb);
+    }
+}).single('');
+
+let imgType= true;
+
+const checkFileType = (file, cb) => {
+    let extName= path.extname(file.originalname);
+
+    if (extName === '.jpg' || extName === '.png') {
+        imgType= true;
+
+        return cb(null, true);
+    } else {
+        imgType= false;
+
+        cb(null, false);
+    }
+};
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.listen(3000, () => console.log('Server listening on port 3000'));
---
+++ database.js
@@ -0,0 +1,38 @@
+const mysql= require('mysql');
+const pool= mysql.createPool({
+    host: 'localhost', 
+    user: 'root',
+    password: '', 
+    database: ''
+});
+
+const getConn= () => {
+    try {
+        return new Promise((resolve, reject) => {
+            pool.getConnection((err, conn) => {
+                if (err) reject(err);
+                else resolve(conn);
+            });
+        });
+    } catch (error) {
+        console.log(error);   
+    }
+};
+
+const executeQuery= (conn, query) => {
+    try {
+        return new Promise((resolve, reject) => {
+            conn.query(query, (err, res) => {
+                if (err) reject(err);
+                else resolve(res); 
+            });
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+module.exports= {
+    'getConn': getConn,
+    'executeQuery': executeQuery
+};
