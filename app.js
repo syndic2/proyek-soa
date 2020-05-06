4dcbb2b9a8cdebb06f63683418a16b7b34094dca
jonathangani279 <jonathangani279@gmail.com>
2020-05-06 21:40:37 +0700

    percobaan hosting

--- app.js
+++ app.js
@@ -41,4 +41,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use('/api', require('./routes/jonsu'));
 
+app.get('/', (req, res) => res.send('Online!'));
+
 app.listen(3000, () => console.log('Server listening on port 3000'));
