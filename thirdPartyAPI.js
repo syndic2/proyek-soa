50516f8d9341700e512c1690b5bf8f39a91e5da2
hubert gunawan <gunawanhubert@gmail.com>
2020-05-21 22:08:11 +0700

    searchByIngridient

--- thirdPartyAPI.js
+++ thirdPartyAPI.js
@@ -1,6 +1,7 @@
 const config= {
     host: 'https://api.spoonacular.com/recipes',
-    api_key: 'f98cdbd8f5084a02894f0ef781e29a61'
+    api_key: 'f98cdbd8f5084a02894f0ef781e29a61',
+    api_key_hubert: '4f8bb17c915842d0b2c28b02853c8a78'
 };
 
 module.exports= config;
